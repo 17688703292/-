a2195f06cd0aa22741e33f64ea8a1875171cee69
RedSky <2389125037@qq.com>
2019-03-20 09:56:06 +0800

    Initial Commit

---
+++ BigMarket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BigMarket
+//
+//  Created by RedSky on 2019/3/20.
+//  Copyright © 2019 RedSky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BigMarket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BigMarket
+//
+//  Created by RedSky on 2019/3/20.
+//  Copyright © 2019 RedSky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
